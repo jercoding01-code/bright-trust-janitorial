841a34f652b599a6fa500bcc7cf3b837493b7e7c
Jerome <jercoding01@gmail.com>
2026-06-29 16:59:30 -0700

    Add STATIC_ROOT for deployment

--- BrightTrustJanitorial/settings.py
+++ BrightTrustJanitorial/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
